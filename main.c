61ed94a33d5085042f1694fd4f414061431f9931
Priya-Nigam <59936557+Priya-Nigam@users.noreply.github.com>
2020-03-25 13:37:51 -0400

    created server c file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//use telnet 127.0.0.1 port to connect
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    return 0;
+}
